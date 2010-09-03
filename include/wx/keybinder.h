4b2ba95d7ebaf278855fd1bf39e0a24e6f68e7c0
verybadsoldier <vbs@springrts.de>
2010-09-03 22:09:12 +0200

    unicode fixes

--- include/wx/keybinder.h
+++ include/wx/keybinder.h
@@ -81,19 +81,19 @@ protected:
 	static wxString discardModifier( const wxString& keystring )
 	{
 		wxString result;
-		if ( keystring.EndsWith("+") )	//handle stuff like numpad+ or ctrl++
+		if ( keystring.EndsWith( wxT("+") ) )	//handle stuff like numpad+ or ctrl++
 		{
 			wxString tmp = keystring;
-			result = tmp.RemoveLast().AfterLast('+') + '+';
+			result = tmp.RemoveLast().AfterLast(wxT('+')) + wxT('+');
 		}
-		else if ( keystring.StartsWith("+") )	//handle stuff like "+ (numpad)"
+		else if ( keystring.StartsWith(wxT("+")) )	//handle stuff like "+ (numpad)"
 		{
 			result = keystring;
 		}
 		else 
 		{
-			size_t lastAdd = keystring.find_last_of('+');
-			if ( ( lastAdd != keystring.npos ) && ( keystring.GetChar(lastAdd - 1) == '+' ) )
+			size_t lastAdd = keystring.find_last_of(wxT('+'));
+			if ( ( lastAdd != keystring.npos ) && ( keystring.GetChar(lastAdd - 1) == wxT('+') ) )
 			{
 				assert( (lastAdd > 0) && "character '+' found in unexcepted location!" );
 				result = keystring.substr( lastAdd );
